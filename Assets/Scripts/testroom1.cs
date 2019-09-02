30f62ad0791ab9e33295f835dba32350ebedfb02
castle <castlez@live.com>
2019-09-02 15:07:13 -0700

    saving

---
+++ Assets/Scripts/testroom1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class testroom1 : MonoBehaviour
+{
+
+    public int move_to_scene = 0;
+
+    public List<PlayerController> Players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Players = new List<PlayerController>();
+
+        // PlayerController player1 = new PlayerController();
+
+        // Players.Add(player1);
+        // Debug.Log("Player 1 joined");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // foreach(PlayerController p in Players)
+        // {
+            
+        // }
+    }
+
+
+}
