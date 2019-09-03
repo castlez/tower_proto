c990313459c8ae75774c20b3738c99c4841b1e02
castle <castlez@live.com>
2019-09-02 17:34:45 -0700

    forgot to add

---
+++ Assets/Scripts/DoorCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorCheck : MonoBehaviour
+{
+    public test_level level;
+    public GameObject scene_object;
+
+    public int coins_left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene_object = GameObject.Find("test_level");
+        level = scene_object.GetComponent<test_level>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coins_left = level.coins;        
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (coins_left == 0)
+        {
+            Debug.Log("Entered Door");
+            SceneManager.LoadScene("test_level2");
+        }
+        else
+        {
+            Debug.Log($"{coins_left} coins left");
+        }
+        
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+   
+    }
+}
